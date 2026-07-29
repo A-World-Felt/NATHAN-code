9883ec2e874b26be291d609f33fc2895be6a2649
Antoine Martin <toni2749@gmail.com>
2026-07-29 12:03:54 -0400

    Sound and SoundScape global strucure

--- game-engine/gameplay_foundations/audio/sound.hpp
+++ game-engine/gameplay_foundations/audio/sound.hpp
@@ -1,25 +1,58 @@
-#ifndef MYGAMEENGINE_SOUND_H
-#define MYGAMEENGINE_SOUND_H
+#ifndef GAME_ENGINE_AUDIO_SOUND_H_
+#define GAME_ENGINE_AUDIO_SOUND_H_
+
+#include "event_bus.hpp"
 
 #include <cstdint>
-#include <vector>
 #include <filesystem>
+#include <queue>
 
 namespace nathan {
-struct Sound {
-    uint8_t id_;
+
+class Sound {
+public:
+    Sound(const std::string_view path_to_file, const float rel_x, const float rel_y, const float rel_z, const float gain)
+        : id_(get_next_id()), path_to_file_(path_to_file), rel_x_(rel_x), rel_y_(rel_y), rel_z_(rel_z), gain_(gain) {}
+
+    uint16_t get_id() const { return id_; }
+
+private:
+    static uint64_t get_next_id() {
+        static std::atomic<uint64_t> next_id{1};
+        return next_id++;
+    }
+
+    uint64_t id_;
     std::filesystem::path path_to_file_;
+    // Relative position needs to be updated continuously if the player moves
     float rel_x_;
     float rel_y_;
     float rel_z_;
-    float gain = 1;
+    float gain_ = 1;
 };
 
-struct SoundScape {
-    std::vector<Sound> begin_sounds_;
-    std::vector<uint8_t> end_sounds_;
+class SoundScape {
+public:
+    explicit SoundScape(EventBus& event_bus);
+
+private:
+    EventBus& event_bus_;
+    std::queue<Sound> begin_sounds_{};
+    std::queue<uint16_t> end_sounds_{};
 };
 
+inline SoundScape::SoundScape(EventBus &event_bus) : event_bus_(event_bus) {
+    event_bus.on_global<Sound>("play sound", [this](const Sound &sound) {
+        // Lock mutex for interprocess communication
+        begin_sounds_.push(sound);
+    });
+
+    event_bus.on_global<Sound>("stop sound", [this](const Sound &sound) {
+        // Lock mutex for interprocess communication
+        end_sounds_.push(sound.get_id());
+    });
 }
 
-#endif //MYGAMEENGINE_SOUND_H
+} // namespace nathan
+
+#endif // GAME_ENGINE_AUDIO_SOUND_H_
