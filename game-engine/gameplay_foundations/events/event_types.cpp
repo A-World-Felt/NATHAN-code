6f71fa818a1142987791eb72a3f4eb279ef2b188
Antoine Martin <mara2040@usherbrooke.ca>
2026-08-03 14:22:38 -0400

    Optimise dynamic_cast with caching
    Cache a pointer to Node3D in SoundEvent constructor instead of calling dynamic_cast every update

--- game-engine/gameplay_foundations/events/event_types.cpp
+++ game-engine/gameplay_foundations/events/event_types.cpp
@@ -1,15 +1,13 @@
 #include "events/event_types.hpp"
 
-#include "game_world/node_3d.hpp"
-
 namespace nathan {
 
 SoundEvent::SoundEvent(EventNode *node, Sound& sound)
-    : node_(node), sound_(&sound) {}
+    : node_(node), node_3d_(dynamic_cast<Node3D*>(node)), sound_(&sound) {}
 
 void SoundEvent::set_rel_position(const Node3D &head_node) const {
-    if (const auto node_3d = dynamic_cast<Node3D*>(node_))
-        sound_->set_rel_position(node_3d->get_transform().get_relative_position_to(head_node.get_transform()));
+    if (node_3d_) {}
+        sound_->set_rel_position(node_3d_->get_transform().get_relative_position_to(head_node.get_transform()));
 }
 
 } // namespace nathan
--- game-engine/gameplay_foundations/events/event_types.hpp
+++ game-engine/gameplay_foundations/events/event_types.hpp
@@ -2,6 +2,7 @@
 #define GAME_ENGINE_EVENTS_EVENT_TYPES_H_
 
 #include "audio/sound.hpp"
+#include "game_world/node_3d.hpp"
 #include "inputs/input_types.hpp"
 
 namespace nathan {
@@ -31,13 +32,14 @@ public:
     void set_rel_position(const Node3D &head_node) const;
 
     EventNode* get_node() const { return node_; }
-    void set_node(EventNode* node) { node_ = node; }
+    void set_node(EventNode* node) { node_ = node; node_3d_ = dynamic_cast<Node3D*>(node); }
 
     Sound* get_sound() const { return sound_; }
     void set_sound(Sound* sound) { sound_ = sound; }
 
 private:
     EventNode* node_;
+    Node3D* node_3d_;
     Sound* sound_;
 };
 
