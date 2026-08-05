c1c845b99f9219ece27f235c3004d292aca23535
Antoine Martin <mara2040@usherbrooke.ca>
2026-08-05 17:32:28 -0400

    Fix curly brackets

--- game-engine/gameplay_foundations/events/event_types.cpp
+++ game-engine/gameplay_foundations/events/event_types.cpp
@@ -8,7 +8,7 @@ SoundEvent::SoundEvent(EventNode *node, Sound& sound)
     : node_(node), node_3d_(dynamic_cast<Node3D*>(node)), sound_(&sound) {}
 
 void SoundEvent::set_rel_position(const Node3D &head_node) const {
-    if (node_3d_) {}
+    if (node_3d_)
         sound_->set_rel_position(node_3d_->get_transform().get_relative_position_to(head_node.get_transform()));
 }
 
